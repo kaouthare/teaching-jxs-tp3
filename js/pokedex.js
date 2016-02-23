15536a38b6dd62b58e9b517330bea4aff2c31a77
kaouthare <kaou.bennouna@gmail.com>
2016-02-23 17:54:35 +0100

    Question3 et 4

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,7 +1,17 @@
 var pokeApp = angular.module('pokedex', ['ngResource']);
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
-    $resourceProvider.defaults.stripTrailingSlashes = false;
-}]);
+    $resourceProvider.defaults.stripTrailingSlashes = false;}]);
 
-var pokeApiUrl = "http://pokeapi.co/"
+pokeApp.controller('repeatController',
+        function($scope){
+            $scope.pokemon=[
+                {id: 1, nom: "picachu"},
+                {id:2, nom: "bulbasor"},
+                {id: 3, nom: "dragon"},
+                {id:4, nom: "charmandar"}
+            ];
+
+    });
+
+var pokeApiUrl = "http://pokeapi.co/";
