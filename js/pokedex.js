ab7ff2f94e5703aa2584a5b625c406bf71fd2e5b
kaou bennouna <kaou.bennouna@gmail.com>
2016-02-24 18:22:25 +0100

    q10, http + filter

--- js/pokedex.js
+++ js/pokedex.js
@@ -4,7 +4,7 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;}]);
 
 pokeApp.controller('repeatController',
-        function($scope,$log,$http){
+        function($scope,$log,$http, $resource){
             $scope.pokemon=[
                 {id: 1, nom: "picachu"},
                 {id:2, nom: "bulbasor"},
@@ -12,10 +12,17 @@ pokeApp.controller('repeatController',
                 {id:4, nom: "charmandar"}
             ];
             $scope.log = $log;
-            $scope.pokemonSelected="blastoise";
-            $http.get("http://www.pokeapi.co/api/v1/pokedex/1/").then(function(response){
+            //$scope.pokemonSelected="blastoise";
+            /*$http.get("http://pokeapi.co/api/v1/pokedex/1").then(function(response){
                 $scope.pokemonSite=response.data.pokemon;
-            });
+            });*/
+            var Pokemons= $resource('http://pokeapi.co/api/v1/type/:id/');
+            Pokemons.get({id:2}, function(pokemon){
+                $log.info(pokemon);
+                $scope.typepok= pokemon.weakness;
+            })
+
+
 
 
     });
