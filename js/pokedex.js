c22f44627876799284bd4f192fbcd2617678130a
kaouthar <kaou.bennouna@gmail.com>
2016-04-30 12:37:03 +0200

    Q15: ajout d'une nouvelle directive

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,15 +1,21 @@
+
+//declaration du module
 var pokeApp = angular.module('pokedex', ['ngResource']);
 
+//configuration du module
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;}]);
 
  var pokeApiUrl = "http://pokeapi.co/";
+
+ //un service qui va permettre de récuperer un pokemon
 pokeApp.factory('searchPokemon', function($resource){
     return $resource("http://pokeapi.co/api/v2/pokemon-species/:id/", {id:'@id'});
 
 });
-
+//le dontroleur appelé dans la page html
 pokeApp.controller('repeatController',
+//la fonction associée au controleur
         function($scope, $log, $http, $resource,searchPokemon){
             /*$scope.pokemon=[
                 {id: 1, nom: "picachu"},
@@ -22,12 +28,15 @@ pokeApp.controller('repeatController',
             $http.get("http://pokeapi.co/api/v1/pokedex/1").then(function(response){
                 $scope.pokemonSite=response.data.pokemon;
             });*/
+
             $scope.log = $log;
+            //récupérer la liste des pokémons
              $http({
                     method: 'GET',
                     url: 'http://pokeapi.co/api/v2/pokedex/1/'
                 }).then(function successCallback(response) {
                     //console.log(response.data)
+                    //mettre la liste dans une variable qui va être utilisée plus tard
                     $scope.pokemonSite = response.data.pokemon_entries;
                 });
                 $scope.go = function(idpoke){
@@ -51,6 +60,12 @@ pokeApp.controller('repeatController',
                 $scope.nompoke= pokemon.name;
                 $scope.typepoks= pokemon.weakness;
             })
+});
+pokeApp.directive('ngPokedex', function() {
+                        return {
+                          restrict: 'A', //Comment on va utiliser la directive (A/E/C/M)
+                          templateUrl: 'pokedex.html' //fichier qui contient notre template html
+                       }
 });
 
 var test ;
